a28956dafffd196ed225ef729b56b231fcb085f2
Emilia Vieira <emilia@landix.com.br>
2019-08-21 08:20:58 -0300

    Doing back

---
+++ back/src/language/language.service.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { LanguageService } from './language.service';
+
+describe('LanguageService', () => {
+  let service: LanguageService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [LanguageService],
+    }).compile();
+
+    service = module.get<LanguageService>(LanguageService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
